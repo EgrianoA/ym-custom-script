a96935820df34bf5ce36a182c2c4a539fa93d00a
Egriano Aristianto <Arisatougri@gmail.com>
2021-11-08 19:09:40 +0700

    Delete the console log
    Delete the console log

--- customScript.js
+++ customScript.js
@@ -4,14 +4,9 @@
     parentStyles.type = 'text/css';
     var parentCssStyles = '#ymDivCircle { bottom: 100px !important; } #ym-notification { bottom: 183px !important; }';
     if (parentStyles.styleSheet) {
-        console.log("A")
         parentStyles.styleSheet.cssText = parentCssStyles;
     } else {
-        console.log("B")
         parentStyles.appendChild(document.createTextNode(parentCssStyles));
-        console.log(parentStyles)
     }
-    console.log(parentStyles)
     parentCssHead.appendChild(parentStyles);
-    console.log(parentCssHead)
 })();
